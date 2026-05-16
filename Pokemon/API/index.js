a4ed29f12c570423d8eb90af5f78029bf5a6ddcf
nqbi <greekgodstud2@gmail.com>
2026-05-17 00:14:36 +0800

    POkemon App added FE and Backend

---
+++ Pokemon/API/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Endpoint 1: A basic health check to see if the server is alive
+app.get("/", (req, res) => {
+  res.send("Pokemon API Server is running beautifully!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🚀 Backend server listening live at http://localhost:${PORT}`);
+});
